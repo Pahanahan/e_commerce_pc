70c2bc35c717c4c628e53acbe67cbcc4aa486638
Pahanahan <korgfd1@gmail.com>
2025-07-26 11:41:53 +0300

    finish MyWishItem, MyWishList components

---
+++ src/pages/Products/components/MyWishList/MyWishItem.jsx
@@ -0,0 +1,115 @@
+import { memo } from "react";
+import { useDispatch } from "react-redux";
+
+import RatingStar from "../../../../components/RatingStar/RatingStar";
+import { toggleLike, addToCart } from "../../../../redux/user/actionCreators";
+
+import check from "../../../../assets/icons/check.svg";
+import call from "../../../../assets/icons/call.svg";
+import cartProduct from "../../../../assets/icons/cart-product.svg";
+import styles from "./MyWishItem.module.css";
+
+const MyWishItem = memo(function MyWishItem({
+  id,
+  rating,
+  availability,
+  image,
+  reviewsCount,
+  description,
+  isLogedIn,
+  likeOrNot,
+  inCartOrNot,
+}) {
+  const dispatch = useDispatch();
+
+  const handleAddLikeOrAddToCart = (value) => {
+    if (!isLogedIn) return;
+
+    const payload = {
+      login: isLogedIn,
+      productId: id,
+    };
+
+    if (value === "like") {
+      dispatch(toggleLike(payload));
+    }
+    if (value === "cart") {
+      dispatch(addToCart(payload));
+    }
+  };
+
+  const ratingStar = Math.round(rating);
+
+  return (
+    <div className={styles["product"]}>
+      <div
+        className={`${styles["product__hover-top"]} ${
+          likeOrNot ? styles["like--active"] : ""
+        }`}
+      >
+        <svg
+          onClick={() => handleAddLikeOrAddToCart("like")}
+          className={styles["product__hover-top-like"]}
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="10" cy="10" r="10" fill="#C94D3F" />
+          <path d="M7 7L13.5 13.5" stroke="white" strokeLinecap="round" />
+          <path d="M13.5 7L7 13.5" stroke="white" strokeLinecap="round" />
+        </svg>
+      </div>
+
+      <div>
+        {availability === "in stock" ? (
+          <div className={styles["product__availability--yes"]}>
+            <img src={check} alt="icon" />
+            in stock
+          </div>
+        ) : (
+          <div className={styles["product__availability--not"]}>
+            <img src={call} alt="icon" />
+            check availability
+          </div>
+        )}
+        <div className={styles["product__image"]}>
+          <a href="#">
+            <img src={image} alt="image" />
+          </a>
+        </div>
+        <div className={styles["product__box"]}>
+          <span className={styles["product__star"]}>
+            <RatingStar rating={ratingStar} />
+          </span>
+          <span className={styles["product__reviews"]}>
+            Reviews ({reviewsCount})
+          </span>
+        </div>
+        <div className={styles["product__text"]}>
+          {description.length < 45
+            ? description
+            : description.slice(0, 40) + "..."}
+        </div>
+      </div>
+
+      <div className={styles["product__hover-bottom"]}>
+        <button
+          onClick={() => handleAddLikeOrAddToCart("cart")}
+          type="button"
+          className={styles["product__cart"]}
+        >
+          <img
+            className={styles["product__cart-img"]}
+            src={cartProduct}
+            alt="cart"
+          />
+          {inCartOrNot ? "Remove" : "Add To Cart"}
+        </button>
+      </div>
+    </div>
+  );
+});
+
+export default MyWishItem;
--- src/pages/Products/components/MyWishList/MyWishList.jsx
+++ src/pages/Products/components/MyWishList/MyWishList.jsx
@@ -1,17 +1,55 @@
 import { useSelector } from "react-redux";
 
+import MyWishItem from "./MyWishItem";
+
 import styles from "./MyWishList.module.css";
 
 function MyWishList() {
-  const isLogedIn = useSelector((state) => state.login.isLogedIn);
-  const wishProducts = useSelector((state) => state.login.users);
-  // console.log(isLogedIn);
-  // console.log(wishProducts);
+  const allProducts = useSelector((state) => state.products.allProducts);
+  const { isLogedIn, users } = useSelector((state) => state.login);
+  const wishProductsLikes =
+    useSelector(
+      (state) =>
+        state.login.users.find((item) => item.login === isLogedIn)?.likes
+    ) || [];
+
+  const wishProducts = allProducts.filter((product) =>
+    wishProductsLikes.includes(product.id)
+  );
+  console.log(wishProducts);
+
+  const wishProductsMap = wishProducts.map((item) => {
+    const findUser = users.find((login) => login.login === isLogedIn);
+
+    const likeOrNot = findUser?.likes?.includes(item.id) || false;
+    const inCartOrNot = findUser?.cart?.includes(item.id) || false;
+
+    return (
+      <MyWishItem
+        key={item.id}
+        id={item.id}
+        rating={item.rating}
+        availability={item.availability}
+        image={item.images[0]}
+        reviewsCount={item.reviewsCount}
+        description={item.description}
+        isLogedIn={isLogedIn}
+        likeOrNot={likeOrNot}
+        inCartOrNot={inCartOrNot}
+      />
+    );
+  });
+
+  console.log(wishProductsMap);
 
   return (
     <div className={styles["wish-list"]}>
       <h2>My Wish List</h2>
-      <p>You have no items in your wish list.</p>
+      {wishProductsLikes.length > 0 ? (
+        wishProductsMap
+      ) : (
+        <p>You have no items in your wish list.</p>
+      )}
     </div>
   );
 }
