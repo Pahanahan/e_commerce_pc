a40791949b66a6198516166dccdb62fee8d1ef62
Pahanahan <korgfd1@gmail.com>
2025-07-28 10:49:39 +0300

    finish More component

--- src/pages/Products/components/More/More.jsx
+++ src/pages/Products/components/More/More.jsx
@@ -1,18 +1,24 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 
 import styles from "./More.module.css";
 
 const More = memo(function More() {
+  const [allText, setAllText] = useState(false);
+
   return (
     <>
-      <div className={styles["fading-text"]}>
-        <p>
+      <div
+        className={
+          allText ? `${styles["normal-text"]}` : `${styles["fading-text"]}`
+        }
+      >
+        <p className={styles["text__visible"]}>
           MSI has unveiled the Prestige Series line of business-class and gaming
           notebooks. Tuned for color accuracy, the Prestige Series also
           leverages True Color Technology, which allows users to adjust the
           display profile to best fit their computing needs.
         </p>
-        <p>
+        <p className={styles["text__visible"]}>
           There are six different screen profiles, which are tuned for gaming,
           reducing eye fatigue, sRGB color accuracy, increasing clarity for
           words and lines, reducing harmful blue light, and optimizing contrast
@@ -23,7 +29,13 @@ const More = memo(function More() {
           reduction. Users working with video or 3D rendering will appreciate
           the "movie mode" for which contrast is increased.
         </p>
-        <p>
+        <p
+          className={
+            allText
+              ? `${styles["text__visible"]}`
+              : `${styles["text__invisible"]}`
+          }
+        >
           Home users or students can benefit from the "anti-blue" and the
           "office mode" options, both of which are designed to reduce eye
           strain. This is helpful when working on the computer for extended
@@ -31,7 +43,16 @@ const More = memo(function More() {
           use the "gamer mode" to increase the screen brightness.
         </p>
       </div>
-      <button className={styles["more__btn"]}>More</button>
+      {allText ? (
+        ""
+      ) : (
+        <button
+          className={styles["more__btn"]}
+          onClick={() => setAllText(true)}
+        >
+          More
+        </button>
+      )}
     </>
   );
 });
