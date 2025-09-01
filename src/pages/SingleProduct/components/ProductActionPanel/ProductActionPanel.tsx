2fb6a9c50108de062722824ec5a127e04423a1f8
Pahanahan <korgfd@yandex.ru>
2025-09-01 21:26:33 +0300

    added ProductActionPanel components

--- src/pages/SingleProduct/components/ProductActionPanel/ProductActionPanel.tsx
+++ src/pages/SingleProduct/components/ProductActionPanel/ProductActionPanel.tsx
@@ -1,7 +1,34 @@
+import arrowUp from "../../../../assets/icons/arrow-up-grey.svg";
+import arrowDown from "../../../../assets/icons/arrow-down-grey.svg";
 import styles from "./ProductActionPanel.module.css";
 
 function ProductActionPanel() {
-  return <div>Action Panel</div>;
+  return (
+    <div className={styles["action-panel"]}>
+      <div className={styles["action-panel__current"]}>
+        <div className={styles["action-panel__current-text"]}>
+          On Sale from $3,299.00
+        </div>
+        <div className={styles["action-panel__current-select"]}>
+          <div className={styles["action-panel__current-num"]}>1</div>
+          <div className={styles["action-panel__current-btns"]}>
+            <button
+              type="button"
+              className={styles["action-panel__current-btn"]}
+            >
+              {arrowUp}
+            </button>
+            <button
+              type="button"
+              className={styles["action-panel__current-btn"]}
+            >
+              {arrowDown}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductActionPanel;
