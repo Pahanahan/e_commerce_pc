10c834d7b8724db95f3c1bca8862f17c4629872e
Pahanahan <korgfd1@gmail.com>
2025-09-10 21:10:21 +0300

    delete log in ProductSlider.tsx, change links to image in data/products-data.json

--- src/pages/SingleProduct/components/Product/ProductSlider/ProductSlider.tsx
+++ src/pages/SingleProduct/components/Product/ProductSlider/ProductSlider.tsx
@@ -30,7 +30,6 @@ const ProductSlider = memo(function ProductSlider({ id }: ProductSliderProp) {
     return allProducts.find((item) => item.id === id);
   }, [allProducts, id]);
   const images: string[] = productById?.images || [defaultImage];
-  console.log(images);
 
   const handleChangeSlide = (num: number): void => {
     setCurrentSlide(num);
